407a0716268cb57e4efa931f5a3ed635210806c6
Vatche Tavitian <vatchetavitian@users.noreply.github.com>
2026-01-27 09:43:47 +0100

    feat: initial import of xs3-cert utility
    Command line tool to retrieve the certificates from a Xesar 3 instance. Offers possibility to be used as a launcher.

---
+++ Sources/xs3-cert/xs3_cert.swift
@@ -0,0 +1,10 @@
+import ArgumentParser
+@main
+@available(macOS 10.15, macCatalyst 13, iOS 13, tvOS 13, watchOS 6, *)
+struct xs3_cert: AsyncParsableCommand {
+    static let configuration = CommandConfiguration(
+        commandName: "xs3-cert",
+        abstract: "An utility for obtaining certificates from an XS3 API.",
+        subcommands: [Version.self,Get.self])
+    init() { }
+}
